983eda74e548346a501421df07dcca44df5b3685
melisguclu <melisguclu26@gmail.com>
2023-12-09 21:14:42 +0300

    add commit function

--- word-masters.js
+++ word-masters.js
@@ -4,6 +4,7 @@ const ANSWER_LENGTH = 5;
 
 async function init(){
     let currentGuess = "";
+    let currentRow = 0;
 
     function addLetter (letter) {
         if(currentGuess.length < ANSWER_LENGTH){
@@ -11,8 +12,18 @@ async function init(){
         } else{ // replace the last letter
             currentGuess = currentGuess.substring(0, currentGuess.length -1) + letter;
         }
-            letters[currentGuess.length - 1].innerText = letter;
+            letters[ ANSWER_LENGTH * currentRow + currentGuess.length - 1].innerText = letter;
         
+    }
+
+    async function commit(){
+        if(currentGuess.length != ANSWER_LENGTH){
+            return;
+        }
+        //first row is complete
+        currentRow++;
+        currentGuess= "";
+
     }
 
 
